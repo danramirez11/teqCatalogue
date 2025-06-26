f2fa0722d9b96e073ada9a15f8d815eb694bf7da
Dan <daramirezp1112@gmail.com>
2025-06-25 21:34:41 -0500

    feat: restructure Home component layout and update button styles for improved UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,19 @@
 import { useNavigate } from "react-router";
+import logo from "../assets/logo-todo-envases.png";
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>home</h1>
-
-      <button className="green" onClick={() => navigate("/envases")}>
-        envases
+    <section className="page homePage">
+      <img src={logo} alt="Logo Todo Envases Y Químicos" />
+      <h3>Bienvenido a nuestro catálogo</h3>
+      <p>¿Qué te gustaría explorar?</p>
+      <button className="blue" onClick={() => navigate("/envases")}>
+        Envases
       </button>
-    </>
+    </section>
   );
 };
 
