2c83a7c9d3eee1aa93660d04ccd1776e83e295c7
Dan <daramirezp1112@gmail.com>
2025-06-26 00:13:55 -0500

    fix: update WhatsApp link in WppCircle component for correct redirection

--- src/components/WppCircle/WppCircle.tsx
+++ src/components/WppCircle/WppCircle.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WppCircle = () => {
   const handleClick = () => {
-    window.open("https://wa.link/wm03ff", "_blank");
+    window.open("https://wa.link/9zxkcu", "_blank");
   };
 
   return (
